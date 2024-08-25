1a563a5ab9249b0d2b8965c8107fb8a9946a58fd
Anil Kumar S <44919646+Alpha-1729@users.noreply.github.com>
2024-08-25 09:25:27 -0700

    Update ex-machina.py
    Resolved TypeError: Float value inside range function

--- Ex-Machina/ex-machina.py
+++ Ex-Machina/ex-machina.py
@@ -3,7 +3,7 @@ import sys
 def sieve(n):
     x = [1] * n
     x[1] = 0
-    for i in range(2,n/2):
+    for i in range(2,n//2):
             j = 2 * i
             while j < n:
                     x[j]=0
